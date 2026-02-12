fa40027ef3dd174636d5d6c2cc3882b4a8d33434
RaptorFF <f.filipovic84@gmail.com>
2026-02-12 13:19:47 +0100

    Creating a Layout

--- app/about/page.js
+++ app/about/page.js
@@ -1,10 +1,7 @@
-import { Navigation } from "../components/Navigation";
-
 export default function About() {
   return (
     <div>
-      <Navigation />
-      <h1>About Wild Oasis</h1>;
+      <h1>About Wild Oasis</h1>
     </div>
   );
 }
