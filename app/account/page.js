2af8978508328b5660a6ffffa0a27d8373704a51
RaptorFF <f.filipovic84@gmail.com>
2026-02-24 12:44:39 +0100

    Building a Custom SignIn Page

--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,17 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest",
 };
 
-export default function Account() {
+export default async function Account() {
+  const session = await auth();
+  const user = session.user.name;
+  const firstName = user.split(" ")[0];
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Wellcome, Fikos
+        Wellcome, {firstName}!
       </h2>
     </div>
   );
