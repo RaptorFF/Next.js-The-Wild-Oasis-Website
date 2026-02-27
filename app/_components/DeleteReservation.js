30c8a36a59b5ff49b8807a27a469520fe4b7acc5
RaptorFF <f.filipovic84@gmail.com>
2026-02-27 01:00:49 +0100

    The useTransition Hook - Another Loading Indicator

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,17 +1,35 @@
 "use client";
 
+import { useTransition } from "react";
 import { deleteReservation } from "../_lib/actions";
 
 import { TrashIcon } from "@heroicons/react/24/solid";
+import SpinnerMini from "./SpinnerMini";
 
 function DeleteReservation({ bookingId }) {
+  // We use startTransition to mark the delete action as non-urgent, allowing React to keep the UI responsive while the deletion is processed.
+  // isPending can be used to show a loading state if needed, but in this case, we simply rely on the UI to remain responsive without blocking.
+  const [isPending, startTransition] = useTransition();
+  function handleDelete() {
+    if (confirm("Are you sure you want to delete this reservation?"))
+      startTransition(() => deleteReservation(bookingId));
+  }
+
   return (
     <button
-      onClick={() => deleteReservation(bookingId)}
+      onClick={handleDelete}
       className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
     >
-      <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
-      <span className="mt-1">Delete</span>
+      {!isPending ? (
+        <>
+          <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+          <span className="mt-1">Delete</span>
+        </>
+      ) : (
+        <span className="mx-auto">
+          <SpinnerMini />
+        </span>
+      )}
     </button>
   );
 }
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -45,22 +45,21 @@ export async function deleteReservation(bookingId) {
   if (!session)
     throw new Error("You must be logged in to delete a reservation");
 
-  // Add logic to delete the reservation with the given bookingId
-  const { error } = await supabase
-    .from("bookings")
-    .delete()
-    .eq("id", bookingId);
-
-  if (error) {
-    throw new Error("You must be logged in to delete a reservation");
-  }
-
   // We also want to ensure that the user can only delete their own reservations, so we check if the bookingId belongs to a reservation made by the currently authenticated user before allowing the deletion to proceed.
   const guestBookings = await getBookings(session.user.guestId);
   const guestBookingIds = guestBookings.map((booking) => booking.id);
   if (!guestBookingIds.includes(bookingId)) {
     throw new Error("You are not allowed to delete this reservation");
   }
+
+  // Add logic to delete the reservation with the given bookingId
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted");
+
   // After deleting the reservation, we want to revalidate the reservations page so that the deleted reservation is no longer displayed when the user navigates back to their reservations.
   revalidatePath("/account/reservations");
 }
