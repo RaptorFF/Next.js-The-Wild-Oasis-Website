eafce43b06ac8db0f15f8d1a803b308c302fa92c
RaptorFF <f.filipovic84@gmail.com>
2026-02-28 23:37:22 +0100

    The useOptimistic Hook - Deleting Reservations Immediately

--- app/_components/ReservationList.js
+++ app/_components/ReservationList.js
@@ -5,6 +5,7 @@ import { useOptimistic } from "react";
 import { deleteReservation } from "../_lib/actions";
 
 function ReservationList({ bookings }) {
+  // We use the useOptimistic hook to manage the optimistic state of the bookings list. This allows us to immediately update the UI when a booking is deleted, without waiting for the server response. The optimisticDelete function will be called when we want to delete a booking from the UI, and it will update the bookings list by removing the deleted booking.
   const [optimisticBookings, optimisticDelete] = useOptimistic(
     bookings,
     (currentBookings, bookingId) => {
