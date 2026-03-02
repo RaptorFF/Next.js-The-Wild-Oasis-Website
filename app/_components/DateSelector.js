280170662a7569cea113a2ed21bdb558be23b5eb
RaptorFF <f.filipovic84@gmail.com>
2026-03-02 13:10:58 +0100

    Finishing the Date Selector

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,6 +1,11 @@
 "use client";
 
-import { isWithinInterval } from "date-fns";
+import {
+  differenceInDays,
+  isPast,
+  isSameDay,
+  isWithinInterval,
+} from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import { useReservation } from "./ReservationContext";
@@ -18,11 +23,11 @@ function isAlreadyBooked(range, datesArr) {
 function DateSelector({ cabin, bookedDates, settings }) {
   const { range, setRange, resetRange } = useReservation();
 
-  // CHANGE
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
+  const displayRange = isAlreadyBooked(range, bookedDates) ? {} : range;
+
+  const { regularPrice, discount } = cabin;
+  const numNights = differenceInDays(displayRange?.to, displayRange?.from); // differenceInDays returns the number of full days between the two dates, so if you select the same day for both from and to, it will return 0, which is what we want for our calculation.
+  const cabinPrice = numNights * (regularPrice - discount);
 
   // SETTINGS
   const { minBookingLength, maxBookingLength } = settings;
@@ -32,7 +37,7 @@ function DateSelector({ cabin, bookedDates, settings }) {
       <DayPicker
         className="pt-12 place-self-center"
         mode="range"
-        selected={range}
+        selected={displayRange}
         onSelect={setRange}
         min={minBookingLength + 1}
         max={maxBookingLength}
@@ -46,6 +51,10 @@ function DateSelector({ cabin, bookedDates, settings }) {
         endMonth={new Date(new Date().getFullYear() + 5, 11)} // December of the year 5 years from now
         captionLayout="dropdown"
         numberOfMonths={2}
+        disabled={(curDate) =>
+          isPast(curDate) ||
+          bookedDates.some((date) => isSameDay(date, curDate))
+        } // Disable past dates and already booked dates and also disable dates that are already booked within the selected range
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
